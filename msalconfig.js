a764459e5a856883132e6ee54b18872bdfedb454
harrykwan <33515754+harrykwan@users.noreply.github.com>
2019-09-24 14:25:56 +0800

    Update msalconfig.js

--- msalconfig.js
+++ msalconfig.js
@@ -1,7 +1,4 @@
 var msalconfig = {
     clientID: "4271889a-03ff-4701-86bf-e40f412c5acb",
     redirectUri: "https://harrykwan.github.io/nasu/main.html"
-}, cache: {
-    cacheLocation: "localStorage",
-    storeAuthStateInCookie: true
 };
